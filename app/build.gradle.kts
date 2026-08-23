a211ff2b45447c9f19d91f8865c00260cd453613
jch0029987-glitch <jch0029987@gmail.com>
2026-08-23 17:40:47 -0500

    Update storeFile path to use rootProject

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         create("release") {
             keyAlias = keystoreProperties.getProperty("keyAlias") ?: "localai-alias"
             keyPassword = keystoreProperties.getProperty("keyPassword")
-            storeFile = keystoreProperties.getProperty("storeFile")?.let { file(it) }
+            storeFile = keystoreProperties.getProperty("storeFile")?.let { rootProject.file(it) }
             storePassword = keystoreProperties.getProperty("storePassword")
         }
     }

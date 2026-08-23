afac8de6a774e2a3467ee89acd58458f5e142b73
jch0029987-glitch <jch0029987@gmail.com>
2026-08-23 13:58:35 -0500

    Add Java toolchain configuration for version 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)

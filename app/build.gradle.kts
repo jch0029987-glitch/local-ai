0e6643145921eecb407c75c51de27d690d8a1e2a
jch0029987-glitch <jch0029987@gmail.com>
2026-08-23 17:20:01 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material
     // Compose BOM
     val composeBom = platform("androidx.compose:compose-bom:2024.04.01")
     implementation(composeBom)

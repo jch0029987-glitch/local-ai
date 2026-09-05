e87f8679ef58ece7eed31239772d31c7cc999c20
jch0029987-glitch <jch0029987@gmail.com>
2026-09-05 17:37:43 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.android") version "2.4.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
     id("com.google.devtools.ksp")
 }
 
@@ -123,7 +123,4 @@ dependencies {
     
     // Optional: If you plan to use root services / IPC
     implementation("com.github.topjohnwu.libsu:service:6.0.0")
-
-
-
 }

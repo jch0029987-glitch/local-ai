601cd9596d9786ba2f0fae5946130e5cc3191175
jch0029987-glitch <jch0029987@gmail.com>
2026-09-05 18:03:59 -0500

    Update Android Gradle plugin version to 8.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.10.0" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false

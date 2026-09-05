969976cd9c034c772745f751d4c1e967f89a01bf
jch0029987-glitch <jch0029987@gmail.com>
2026-09-05 17:56:02 -0500

    Update KSP plugin version to 2.4.0-1.0.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.google.devtools.ksp") version "2.4.0-1.0.25" apply false
 }
